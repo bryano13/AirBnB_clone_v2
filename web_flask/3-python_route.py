7fc0ca586fede903bfadf03518a2498356c5e863
bryano13 <bryano13@hotmail.com>
2020-09-01 19:07:48 -0500

    6

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+"""This module starts a Flask web application"""
+
+from flask import Flask, escape
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_world():
+    """Function that diplays Hello HBNB"""
+
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hello_hbnb():
+    """Function that diplays HBNB"""
+
+    return 'HBNB'
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c_route(text):
+    """Function that displays “C ” followed by a string"""
+
+    text = escape(text)
+    return "C {}".format(text.replace("_", " "))
+
+
+@app.route('/python', strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def python_route(text="is cool"):
+    """Function that displays “Python ” followed by a string"""
+
+    text = escape(text)
+    return "Python {}".format(text.replace('_', ' '))
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
